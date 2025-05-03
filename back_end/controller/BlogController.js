004cbc0a5aacccf81911b533667e4f45af3a0fef
Dilshan Randika Sandaruwan <randikasandaruwan917@gmail.com>
2025-05-03 17:17:53 +0530

    createBlog part add to BlogController

--- back_end/controller/BlogController.js
+++ back_end/controller/BlogController.js
@@ -1,1 +1,20 @@
 create/delete/search/update
+const BlogSchema = require('../models/BlogModel'); // Assuming you have a Blog model defined
+
+const createBlog = (req, res) => {
+    const { title, content, image, by } = req.body;
+
+    if (!title || !content || !Image || !by) {
+        return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    const newBlog = new BlogSchema({ title, content, image, by }); 
+    // by before should set using authentication middleware
+
+    newBlog.save()
+        .then(blog => res.status(201).json(blog))
+        .catch(err => res.status(500).json({ message: 'Error creating blog', error: err }));
+}
+
+
+module.exports = {createBlog};
