54290cb4c9bfbd36ce71f9e6ebcdca052d70b843
Dilshan Randika Sandaruwan <randikasandaruwan917@gmail.com>
2025-05-21 15:27:10 +0530

    cloudinary version changed

--- back_end/middleware/verifyToken.js
+++ back_end/middleware/verifyToken.js
@@ -8,7 +8,7 @@ const verifyToken = (req, res, next) => {
 
   try {
     const verified = jwt.verify(token, process.env.jwt_secret);
-    req.user = verified; // attach user to request 
+    req.user = verified; // attach user to request
     next();
   } catch (err) {
     res.status(401).json({ message: "Invalid Token" });
