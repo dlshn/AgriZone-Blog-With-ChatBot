dde504c253e9c5b6d6719bb303e4328180a4b41a
Dilshan Randika Sandaruwan <randikasandaruwan917@gmail.com>
2025-06-03 06:29:08 +0530

    Add media page with youtube videos link

--- front_end/src/Home.jsx
+++ front_end/src/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Hero} from './components/Hero';
 import { Article } from './components/Article';
+import Media from './components/Media'
 
 const Home = () => {
   return (
     <>
     <Hero/>
     <Article/>
+    <Media/>
     </>
   )
 }
